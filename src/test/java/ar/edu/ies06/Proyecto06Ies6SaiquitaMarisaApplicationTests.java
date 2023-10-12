48fa8b6a1239fa376d360bc573a7a74b17b6afd6
EXO <EXO@DESKTOP-EI6B9KV>
2023-10-12 16:02:06 -0300

    Proyecto06 Saiquita_Marisa

---
+++ src/test/java/ar/edu/ies06/Proyecto06Ies6SaiquitaMarisaApplicationTests.java
@@ -0,0 +1,13 @@
+package ar.edu.ies06;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Proyecto06Ies6SaiquitaMarisaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
